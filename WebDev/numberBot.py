151463cf2fa1e6b77b87d02d7ce97557eeed25e2
Kelly Mahaffy <kelly.mahaffy@uconn.edu>
2020-08-02 14:06:57 -0400

    Organization of the folders, KM

---
+++ WebDev/numberBot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+with open('asdf.xml', 'r') as file:
+    filedata = file.read()
+
+for x in range(3902,6000,1):
+    filedata = filedata.replace('\"wynken1509_\"', '\"wynken1509_' + str(x) + '\"',1)
+
+with open('asdf.xml', 'w') as file:
+    file.write(filedata)
---
+++ WebDev/renumber.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+with open('asdf.xml', 'r') as file:
+    filedata = file.read()
+
+for x in range(3901,1344,-1):
+    filedata = filedata.replace('\"wynken1509_' + str(x), '\"wynken1509_' + str(x + 1))
+
+with open('asdf.xml', 'w') as file:
+    file.write(filedata)
+
+
+
